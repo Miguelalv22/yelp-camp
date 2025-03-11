6f673504f9237fe65f476b70da165f3d9e94640e
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-03-11 12:17:52 -0600

    Se modifican las seeds para que haya una descripcion y una imagen aleatoria en el campamento.

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const randomNumber = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
             location: `${cities[randomNumber].city}, ${cities[randomNumber].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut eaque hic cumque quidem atque, adipisci, reiciendis facere quas labore nulla neque vel corporis deserunt nihil nam voluptas quis facilis. Vero.',
+            price
         })
         await camp.save()
     }
