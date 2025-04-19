1e367af49076d6f70c931b43af22b2d1f5858221
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-04-18 18:09:58 -0600

    Se configura el archivo app para que puedan almacenarse nuevos usuarios por medio de passport

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const campgrounds = require('./routes/campgrounds')
 const reviews = require('./routes/reviews');
+const user = require("./models/user");
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -39,7 +43,14 @@ const sessionConfig = {
     }
 }
 app.use(session(sessionConfig));
-app.use(flash())
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
