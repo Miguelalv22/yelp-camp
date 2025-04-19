b4a65565a192024f9f628455cd266b58d4a71c9f
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-04-18 19:01:35 -0600

    Se crea una ruta para el registro del usuario, se crea la ventana de registro y se actualizan rutas de campground y review

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds')
-const reviews = require('./routes/reviews');
-const user = require("./models/user");
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds')
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -58,8 +58,15 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+// app.get('/fakeUser', async (req, res) => {
+//     const user = new User({ email: 'something@gmail.com', username: 'Someone' });
+//     const newUser = await User.register(user, 'chicken');
+//     res.send(newUser);
+// })
+
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
     res.render('home');
