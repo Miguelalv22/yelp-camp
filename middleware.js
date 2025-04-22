71423102ff8279577208cc088bae9b491fafe4dc
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-04-22 13:41:56 -0600

    Se modifico el esquema de reviews para poder mostrar el autor del review. Se crea el middleware y agrego a las rutas de review para que solo el autor pueda eliminar y al postear se pase la informacion del usuario. Se pasan los valores atravez de la ruta de campgrounds pata poder mostrar el autor del review y solo mostrar el boton de Delete si el creador esta logeado

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 
 module.exports.storeReturnTo = (req, res, next) => {
     if (req.session.returnTo) {
@@ -32,6 +33,16 @@ module.exports.isAuthor = async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
         req.flash('error', 'You do not have permission');
         return res.redirect(`/campgrounds/${id}`);
     }
